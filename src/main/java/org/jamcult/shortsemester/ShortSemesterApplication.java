3e1e2659d7fd0ae23385509e194a9b78ffddaaf9
CyberThink777 <alexanderm61@gmail.com>
2020-08-26 20:42:46 +0700

    enabled spring-security

--- src/main/java/org/jamcult/shortsemester/ShortSemesterApplication.java
+++ src/main/java/org/jamcult/shortsemester/ShortSemesterApplication.java
@@ -2,8 +2,10 @@ package org.jamcult.shortsemester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ShortSemesterApplication {
 
 	public static void main(String[] args) {
